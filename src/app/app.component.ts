f68ffa4f3377abd0b2833e049df8051cafb5c1bd
JaavLex <alexandre.javet@hotmail.com>
2021-09-27 16:07:49 +0200

    [feat] Can now naviguate through the APOD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'kataapod';
-  appTitleContent: string = "APOD Navigator / Browser"
+  appTitleContent: string = "APOD Navigator / Browser";
+  todayDate: Date = new Date();
+  selectedDateUnformatted: Date = this.todayDate;
+  selectedDate: string = "";
+
+  formatDate(date: Date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
+  
+  previousDay() {
+    this.selectedDateUnformatted.setDate(this.selectedDateUnformatted.getDate() - 1)
+    this.selectedDate = this.formatDate(this.selectedDateUnformatted);
+  }
+
+  nextDay() {
+    this.selectedDateUnformatted.setDate(this.selectedDateUnformatted.getDate() + 1)
+    this.selectedDate = this.formatDate(this.selectedDateUnformatted);
+  }
 }
