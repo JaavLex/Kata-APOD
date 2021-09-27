4d94a8f23f6da62069eb4ef9509f144bd37b8273
JaavLex <alexandre.javet@hotmail.com>
2021-09-27 09:48:38 +0200

    [feat] Added api fetching module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { ApodDescComponent } from './apod-desc/apod-desc.component';
+import { ApodTitleComponent } from './apod-title/apod-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageTitleComponent,
+    ApodDescComponent,
+    ApodTitleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
