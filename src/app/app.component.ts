d5254ff38aa7ba2e58be81eb303971e990289a75
JaavLex <alexandre.javet@hotmail.com>
2021-09-27 09:50:05 +0200

    [feat] Added component to the page (TESTING)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kataapod';
+  title: string = 'kataapod';
+  appTitleContent: string = "APOD Navigator / Browser"
 }
