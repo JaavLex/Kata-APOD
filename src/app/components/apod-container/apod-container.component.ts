41b9c8804cbec4f50ff198bb860d077ad047a888
JaavLex <alexandre.javet@hotmail.com>
2021-10-01 11:46:43 +0200

    [feat] Now sets directly our service's URL and transfers it to child components

--- src/app/components/apod-container/apod-container.component.ts
+++ src/app/components/apod-container/apod-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-apod-container',
@@ -7,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ApodContainerComponent implements OnInit {
   @Input() apiDate: string = "";
+  @Input() apiService: ApiService = new ApiService();
   @Input() isAPIKeyPresent: boolean = false;
   @Output() apiKey = new EventEmitter();
   
