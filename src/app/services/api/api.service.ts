f06458a878fe4b39fd3bdc68fb62f7776476f7dd
JaavLex <alexandre.javet@hotmail.com>
2021-10-01 11:45:49 +0200

    [feat] Improving api service (fetch now works ! and using manager instead of multiple functions)

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,24 +1,50 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  API_KEY: string | null = "null";
+  API_KEY: string | null | undefined = null;
+  API_URL: string | null | undefined = null;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  setApiKey(apiKey: string | null) {
-    this.API_KEY = apiKey;
+  apiKeyManager(managerRequest: 'get' | 'set', apiKey?: string | null): void | string | null {
+    switch(managerRequest) {
+      case 'get':
+        return this.API_KEY
+      case 'set':
+        if (apiKey)
+          this.API_KEY = apiKey
+        else
+          throw new Error('ERROR: apiKey is empty or null, you must specify it')
+        break;
+      default:
+        throw new Error('ERROR: Type of request is unknown')
+    }
   }
 
-  getApiData(date: string): any {
-    if (this.API_KEY == "null") {
-      fetch(`https://api.nasa.gov/planetary/apod?api_key=${this.API_KEY}&date=${date}`)
-        .then(data => {return data})
+  apiUrlManager(managerRequest: 'get' | 'set', apiUrl?: string | null): void | string | null {
+    switch(managerRequest) {
+      case 'get':
+        return this.API_URL
+      case 'set':
+        if (apiUrl)
+          this.API_URL = apiUrl
+        else
+          throw new Error('ERROR: apiUrl is empty or null, you must specify it')
+        break;
+      default:
+        throw new Error('ERROR: Type of request is unknown')
+    }
+  }
+
+  async getApiData(apiDate: string): Promise<any> {
+    if (this.API_KEY) {
+      return await fetch(`${this.API_URL}?api_key=${this.API_KEY}&date=${apiDate}`)
+        .then(data => {return data.json()})
     } else {
-      throw new Error(`You MUST have set an api key with 'setApiKey()' before getting data`)
+      throw new Error(`ERROR: You MUST have set an api key with 'setApiKey()' before getting data`)
     }
   }
 }
