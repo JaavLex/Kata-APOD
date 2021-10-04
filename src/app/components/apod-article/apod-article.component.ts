a9380a600d32f4e727bf6e1910a59495a9183562
JaavLex <alexandre.javet@hotmail.com>
2021-10-04 09:40:29 +0200

    [feat] ApiService now works ! All api request are made via a custom service

--- src/app/components/apod-article/apod-article.component.ts
+++ src/app/components/apod-article/apod-article.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
@@ -9,34 +8,17 @@ import { ApiService } from 'src/app/services/api/api.service';
 })
 export class ApodArticleComponent implements OnInit {
   apodData: any = null;
-  apodCopyright: string = "";
-  apodExplanation: string = "";
-  apodMedia: string = "";
-  apodMediaType: string = "";
-  apodTitle: string = "";
-  apodDate: string = "";
   @Input() apiService: ApiService = new ApiService();
   @Input() apiDate: string = "";
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   async apiDataSet(): Promise<any> {
-    console.log(await this.apiService.getApiData(this.apodDate))
-    this.apodData = await this.apiService.getApiData(this.apodDate);
-    //return this.apiService.getApiData(this.apodDate);
-    // this.http.get<any>(`https://api.nasa.gov/planetary/apod?api_key=${localStorage.getItem('jaavlex-apod-api-key')}&date=${this.apiDate}`).subscribe(data => {
-    //   data.copyright && (this.apodCopyright = data.copyright);
-    //   this.apodExplanation = data.explanation;
-    //   this.apodMedia = data.url;
-    //   this.apodMediaType = data.media_type;
-    //   this.apodTitle = data.title;
-    //   this.apodDate = data.date;
-    // })
+    this.apodData = await this.apiService.getApiData(this.apiDate);
   }
   
   ngOnInit(): void {
     this.apiService.apiKeyManager('set', localStorage.getItem('jaavlex-apod-api-key'))
-    //alert(this.apiService.apiKeyManager('get'))
     this.apiDataSet();
   }
 
